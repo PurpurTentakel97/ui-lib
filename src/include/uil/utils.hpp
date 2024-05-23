8adcc7f400d4300eb9cdb855175f861e373db099
Martin <martin.schreurs@netcologne.de>
2024-05-23 22:54:42 +0200

    add utils

---
+++ src/include/uil/utils.hpp
@@ -0,0 +1,16 @@
+//
+// Purpur Tentakel
+// 23.05.24
+//
+
+#pragma once
+
+namespace uil {
+    struct Rect final {
+        float x;
+        float y;
+        float width;
+        float height;
+    };
+
+} // namespace uil
