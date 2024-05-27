f6f2e2aea3344be554babf26cfa62ff7ac39f799
martin <martin.schreurs@netcologne.de>
2024-05-27 12:56:05 +0200

    exception test

---
+++ test/test_exception.cpp
@@ -0,0 +1,37 @@
+//
+// Purpur Tentakel
+// 27.05.24
+//
+
+#include <gtest/gtest.h>
+#include <uil/exception.hpp>
+
+TEST(Exception, CallbackException) {
+    try {
+        throw uil::CallbackException("some callback fail");
+    } catch (std::runtime_error const& e) {
+        EXPECT_STREQ(e.what(), "some callback fail");
+    } catch ([[maybe_unused]] std::exception const& e) {
+        GTEST_FAIL() << "uil::CallbackException is no std::runtime_error";
+    }
+}
+
+TEST(Exception, BadResolution) {
+    try {
+        throw uil::BadResolution("some resolution fail");
+    } catch (std::logic_error const& e) {
+        EXPECT_STREQ(e.what(), "some resolution fail");
+    } catch ([[maybe_unused]] std::exception const& e) {
+        GTEST_FAIL() << "uil::CallbackException is no std::logic_error";
+    }
+}
+
+TEST(Exception, BadAlignment) {
+    try {
+        throw uil::BadAlignment("some alignment fail");
+    } catch (std::logic_error const& e) {
+        EXPECT_STREQ(e.what(), "some alignment fail");
+    } catch ([[maybe_unused]] std::exception const& e) {
+        GTEST_FAIL() << "uil::CallbackException is no std::logic_error";
+    }
+}
