c41ac2254642de7364199f2a19c94e1e4df47c8c
Martin <martin.schreurs@netcologne.de>
2024-07-12 22:27:53 +0200

    operator for Vector2

--- src/include/uil/helper_vec.hpp
+++ src/include/uil/helper_vec.hpp
@@ -5,9 +5,52 @@
 
 #pragma once
 #include <raylib.h>
+#include <uil/exception.hpp>
 
 namespace uil {
     [[nodiscard]] float magnitude(Vector2 const& vec);
 
     [[nodiscard]] Vector2 normalize(Vector2 const& vec);
 } // namespace uil
+inline Vector2& operator+=(Vector2& lhs, Vector2 const& rhs) {
+    lhs.x += rhs.x;
+    lhs.y += rhs.y;
+    return lhs;
+}
+[[nodiscard]] inline Vector2 operator+(Vector2 lhs, Vector2 const& rhs) {
+    lhs += rhs;
+    return lhs;
+}
+
+inline Vector2& operator-=(Vector2& lhs, Vector2 const& rhs) {
+    lhs.x -= rhs.x;
+    lhs.y -= rhs.y;
+    return lhs;
+}
+[[nodiscard]] inline Vector2 operator-(Vector2 lhs, Vector2 const& rhs) {
+    lhs -= rhs;
+    return lhs;
+}
+
+inline Vector2 operator*=(Vector2& lhs, float const rhs) {
+    lhs.x *= rhs;
+    lhs.y *= rhs;
+    return lhs;
+}
+[[nodiscard]] inline Vector2 operator*(Vector2 lhs, float const rhs) {
+    lhs *= rhs;
+    return lhs;
+}
+
+inline Vector2 operator/=(Vector2& lhs, float const rhs) {
+    if (rhs == 0.0f) {
+        throw uil::DivideByZero("Devide by zero while deviding a vector by a float");
+    }
+    lhs.x /= rhs;
+    lhs.y /= rhs;
+    return lhs;
+}
+[[nodiscard]] inline Vector2 operator/(Vector2 lhs, float const rhs) {
+    lhs /= rhs;
+    return lhs;
+}
--- test/test_vec.cpp
+++ test/test_vec.cpp
@@ -48,11 +48,135 @@ INSTANTIATE_TEST_SUITE_P(VECTOR2,
                                          std::make_tuple(Vector2(0.0f, 1.0f), Vector2(0.0f, 1.0f)),
                                          std::make_tuple(Vector2(2.0f, 1.0f), Vector2(0.8944272f, 0.4472136f))));
 
-TEST(Vector2, DevideByZeroExpection) {
+TEST(Vector2, DevideByZeroExpectionNormalize) {
     auto constexpr vec = Vector2{ 0.0f, 0.0f };
 
     try {
         [[maybe_unused]] auto const result = uil::normalize(vec);
         GTEST_FAIL() << "DevideByZero not thrown";
     } catch (uil::DivideByZero const& e) { EXPECT_STREQ(e.what(), "Devide by 0.0f while normalize a Vector2"); }
+}
+
+class Vector2OperatorPlusFixures : public testing::TestWithParam<std::tuple<Vector2, Vector2, Vector2>> { };
+
+TEST_P(Vector2OperatorPlusFixures, Succsess) {
+    auto const vec_c    = std::get<0>(GetParam());
+    auto vec_m          = std::get<0>(GetParam());
+    auto const to_add   = std::get<1>(GetParam());
+    auto const expected = std::get<2>(GetParam());
+
+    auto const result = vec_c + to_add;
+    vec_m += to_add;
+
+    EXPECT_FLOAT_EQ(result.x, expected.x);
+    EXPECT_FLOAT_EQ(result.y, expected.y);
+    EXPECT_FLOAT_EQ(vec_m.x, expected.x);
+    EXPECT_FLOAT_EQ(vec_m.y, expected.y);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+        VECTOR2,
+        Vector2OperatorPlusFixures,
+        ::testing::Values(std::make_tuple(Vector2{ 1.0f, 1.0f }, Vector2{ 1.0f, 1.0f }, Vector2{ 2.0f, 2.0f }),
+                          std::make_tuple(Vector2{ 1.0f, 1.0f }, Vector2{ -1.0f, -1.0f }, Vector2{ 0.0f, 0.0f }),
+                          std::make_tuple(Vector2{ -1.0f, -1.0f }, Vector2{ 1.0f, 1.0f }, Vector2{ 0.0f, 0.0f }),
+                          std::make_tuple(Vector2{ -1.0f, -1.0f }, Vector2{ -1.0f, -1.0f }, Vector2{ -2.0f, -2.0f }),
+                          std::make_tuple(Vector2{ 1.0f, 1.0f }, Vector2{ 0.0f, 0.0f }, Vector2{ 1.0f, 1.0f })));
+
+class Vector2OperatorMinusFixtures : public testing::TestWithParam<std::tuple<Vector2, Vector2, Vector2>> { };
+
+TEST_P(Vector2OperatorMinusFixtures, Succsess) {
+    auto const vec_c    = std::get<0>(GetParam());
+    auto vec_m          = std::get<0>(GetParam());
+    auto const to_sub   = std::get<1>(GetParam());
+    auto const expected = std::get<2>(GetParam());
+
+    auto const result = vec_c - to_sub;
+    vec_m -= to_sub;
+
+    EXPECT_FLOAT_EQ(result.x, expected.x);
+    EXPECT_FLOAT_EQ(result.y, expected.y);
+    EXPECT_FLOAT_EQ(vec_m.x, expected.x);
+    EXPECT_FLOAT_EQ(vec_m.y, expected.y);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+        VECTOR2,
+        Vector2OperatorMinusFixtures,
+        ::testing::Values(std::make_tuple(Vector2{ 1.0f, 1.0f }, Vector2{ 1.0f, 1.0f }, Vector2{ 0.0f, 0.0f }),
+                          std::make_tuple(Vector2{ 1.0f, 1.0f }, Vector2{ -1.0f, -1.0f }, Vector2{ 2.0f, 2.0f }),
+                          std::make_tuple(Vector2{ -1.0f, -1.0f }, Vector2{ 1.0f, 1.0f }, Vector2{ -2.0f, -2.0f }),
+                          std::make_tuple(Vector2{ -1.0f, -1.0f }, Vector2{ -1.0f, -1.0f }, Vector2{ 0.0f, 0.0f }),
+                          std::make_tuple(Vector2{ 1.0f, 1.0f }, Vector2{ 0.0f, 0.0f }, Vector2{ 1.0f, 1.0f })));
+
+class Vector2OperatorMultiplyFixtures : public testing::TestWithParam<std::tuple<Vector2, float, Vector2>> { };
+
+TEST_P(Vector2OperatorMultiplyFixtures, Succsess) {
+    auto const vec_c       = std::get<0>(GetParam());
+    auto vec_m             = std::get<0>(GetParam());
+    auto const to_multiply = std::get<1>(GetParam());
+    auto const expected    = std::get<2>(GetParam());
+
+    auto const result = vec_c * to_multiply;
+    vec_m *= to_multiply;
+
+    EXPECT_FLOAT_EQ(result.x, expected.x);
+    EXPECT_FLOAT_EQ(result.y, expected.y);
+    EXPECT_FLOAT_EQ(vec_m.x, expected.x);
+    EXPECT_FLOAT_EQ(vec_m.y, expected.y);
+}
+
+INSTANTIATE_TEST_SUITE_P(VECTOR2,
+                         Vector2OperatorMultiplyFixtures,
+                         ::testing::Values(std::make_tuple(Vector2{ 1.0f, 1.0f }, 2.0f, Vector2{ 2.0f, 2.0f }),
+                                           std::make_tuple(Vector2{ 1.0f, 1.0f }, -2.0f, Vector2{ -2.0f, -2.0f }),
+                                           std::make_tuple(Vector2{ -1.0f, -1.0f }, 2.0f, Vector2{ -2.0f, -2.0f }),
+                                           std::make_tuple(Vector2{ -1.0f, -1.0f }, -2.0f, Vector2{ 2.0f, 2.0f }),
+                                           std::make_tuple(Vector2{ 1.0f, 1.0f }, 1.0f, Vector2{ 1.0f, 1.0f }),
+                                           std::make_tuple(Vector2{ 1.0f, 1.0f }, 0.0f, Vector2{ 0.0f, 0.0f })));
+
+class Vector2OperatorDivideFixtures : public testing::TestWithParam<std::tuple<Vector2, float, Vector2>> { };
+
+TEST_P(Vector2OperatorDivideFixtures, Succsess) {
+    auto const vec_c     = std::get<0>(GetParam());
+    auto vec_m           = std::get<0>(GetParam());
+    auto const to_divide = std::get<1>(GetParam());
+    auto const expected  = std::get<2>(GetParam());
+
+    auto const result = vec_c / to_divide;
+    vec_m /= to_divide;
+
+    EXPECT_FLOAT_EQ(result.x, expected.x);
+    EXPECT_FLOAT_EQ(result.y, expected.y);
+    EXPECT_FLOAT_EQ(vec_m.x, expected.x);
+    EXPECT_FLOAT_EQ(vec_m.y, expected.y);
+}
+
+
+INSTANTIATE_TEST_SUITE_P(VECTOR2,
+                         Vector2OperatorDivideFixtures,
+                         ::testing::Values(std::make_tuple(Vector2{ 1.0f, 1.0f }, 2.0f, Vector2{ 0.5f, 0.5f }),
+                                           std::make_tuple(Vector2{ 1.0f, 1.0f }, -2.0f, Vector2{ -0.5f, -0.5f }),
+                                           std::make_tuple(Vector2{ -1.0f, -1.0f }, 2.0f, Vector2{ -0.5f, -0.5f }),
+                                           std::make_tuple(Vector2{ -1.0f, -1.0f }, -2.0f, Vector2{ 0.5f, 0.5f }),
+                                           std::make_tuple(Vector2{ 1.0f, 1.0f }, 1.0f, Vector2{ 1.0f, 1.0f })));
+
+TEST(Vector2, DevideByZeroExpectionDivide) {
+    auto constexpr vec_c = Vector2{ 1.0f, 1.0f };
+    auto vec_m           = Vector2{ 1.0f, 1.0f };
+    auto constexpr f     = 0.0f;
+
+    try {
+        [[maybe_unused]] auto const result = vec_c / f;
+        GTEST_FAIL() << "DevideByZero not thrown while operator/";
+    } catch (uil::DivideByZero const& e) {
+        EXPECT_STREQ(e.what(), "Devide by zero while deviding a vector by a float");
+    }
+
+    try {
+        vec_m /= f;
+        GTEST_FAIL() << "DevideByZero not thrown while operator/=";
+    } catch (uil::DivideByZero const& e) {
+        EXPECT_STREQ(e.what(), "Devide by zero while deviding a vector by a float");
+    }
 }
