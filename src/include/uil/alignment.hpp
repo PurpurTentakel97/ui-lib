a02497a7cf5f00f6cac1028799dadef25eac2b32
Martin <martin.schreurs@netcologne.de>
2024-07-14 15:26:38 +0200

    alignment for position vector

--- src/include/uil/alignment.hpp
+++ src/include/uil/alignment.hpp
@@ -42,6 +42,25 @@ namespace uil {
     [[nodiscard]] Rectangle vertical_aligned_position(Rectangle relative, VerticalAlignment alignment);
     [[nodiscard]] Rectangle vertical_aligned_position_reversed(Rectangle relative, VerticalAlignment alignment);
 
+    [[nodiscard]] Vector2 aligned_position(Vector2 const& point, Vector2 const& size, Alignment const& alignment);
+    [[nodiscard]] Vector2 aligned_position_reversed(Vector2 const& point,
+                                                    Vector2 const& size,
+                                                    Alignment const& alignment);
+
+    [[nodiscard]] Vector2 horizontal_aligned_position(Vector2 const& point,
+                                                               Vector2 const& size,
+                                                               HorizontalAlignment const& alignment);
+    [[nodiscard]] Vector2 horizontal_aligned_position_reversed(Vector2 const& point,
+                                                               Vector2 const& size,
+                                                               HorizontalAlignment const& alignment);
+
+    [[nodiscard]] Vector2 vertical_aligned_position(Vector2 const& point,
+                                                             Vector2 const& size,
+                                                             VerticalAlignment const& alignment);
+    [[nodiscard]] Vector2 vertical_aligned_position_reversed(Vector2 const& point,
+                                                             Vector2 const& size,
+                                                             VerticalAlignment const& alignment);
+
     [[nodiscard]] HorizontalAlignment to_horizontal_alignment(Alignment alignment);
     [[nodiscard]] VerticalAlignment to_vertical_alignment(Alignment alignment);
     [[nodiscard]] Alignment to_alignment(VerticalAlignment vertical_alignment,
--- test/test_alignment_vertical.cpp
+++ test/test_alignment_vertical.cpp
@@ -93,8 +93,8 @@ TEST(Alignment, VerticalExceptionReversed) {
     auto constexpr value = static_cast<uil::VerticalAlignment>(100);
 
     try {
-        [[maybe_unused]] auto const result =
-                uil::vertical_aligned_position_reversed(Rectangle{ 0.0f, 0.0f, 0.0f, 0.0f }, value);
+        [[maybe_unused]] auto const result
+                = uil::vertical_aligned_position_reversed(Rectangle{ 0.0f, 0.0f, 0.0f, 0.0f }, value);
         GTEST_FAIL() << "expection uil::BadAlignment not thrown";
     } catch (uil::BadAlignment const& e) {
         EXPECT_STREQ(e.what(), "invalid vertical alignmend enum while reversed aligning rectanlge verticaly");
@@ -118,6 +118,92 @@ TEST_P(DoubleVerticalAlignmentFixtures, DobleHorizontalCovert) {
 
 INSTANTIATE_TEST_SUITE_P(ALIGNMENT,
                          DoubleVerticalAlignmentFixtures,
+                         testing::Values(uil::VerticalAlignment::Top,
+                                         uil::VerticalAlignment::Mid,
+                                         uil::VerticalAlignment::Bottom));
+
+class VerticalAlignmentFicturesVertical
+    : public testing::TestWithParam<std::tuple<Vector2, Vector2, uil::VerticalAlignment, Vector2>> { };
+
+TEST_P(VerticalAlignmentFicturesVertical, Succsess) {
+    auto const pos       = std::get<0>(GetParam());
+    auto const size      = std::get<1>(GetParam());
+    auto const alignment = std::get<2>(GetParam());
+    auto const expected  = std::get<3>(GetParam());
+
+    auto const result = uil::vertical_aligned_position(pos, size, alignment);
+
+    EXPECT_FLOAT_EQ(expected.x, result.x);
+    EXPECT_FLOAT_EQ(expected.y, result.y);
+    EXPECT_FLOAT_EQ(pos.x, result.x);
+}
+
+INSTANTIATE_TEST_SUITE_P(ALIGNMENT,
+                         VerticalAlignmentFicturesVertical,
+                         ::testing::Values(std::make_tuple(Vector2{ 0.25f, 0.25f },
+                                                           Vector2{ 0.5f, 0.5f },
+                                                           uil::VerticalAlignment::Top,
+                                                           Vector2{ 0.25f, 0.25f }),
+                                           std::make_tuple(Vector2{ 0.25f, 0.5f },
+                                                           Vector2{ 0.5f, 0.5f },
+                                                           uil::VerticalAlignment::Mid,
+                                                           Vector2{ 0.25f, 0.25f }),
+                                           std::make_tuple(Vector2{ 0.25f, 0.75f },
+                                                           Vector2{ 0.5f, 0.5f },
+                                                           uil::VerticalAlignment::Bottom,
+                                                           Vector2{ 0.25f, 0.25f })));
+
+
+class VectorAlignmentFicturesVerticalReversed
+    : public testing::TestWithParam<std::tuple<Vector2, Vector2, uil::VerticalAlignment, Vector2>> { };
+
+TEST_P(VectorAlignmentFicturesVerticalReversed, Success) {
+    auto const pos       = std::get<0>(GetParam());
+    auto const size      = std::get<1>(GetParam());
+    auto const alignment = std::get<2>(GetParam());
+    auto const expected  = std::get<3>(GetParam());
+
+    auto const result = uil::vertical_aligned_position_reversed(pos, size, alignment);
+
+    EXPECT_FLOAT_EQ(expected.x, result.x);
+    EXPECT_FLOAT_EQ(expected.y, result.y);
+    EXPECT_FLOAT_EQ(pos.x, result.x);
+}
+
+INSTANTIATE_TEST_SUITE_P(ALIGNMENT,
+                         VectorAlignmentFicturesVerticalReversed,
+                         ::testing::Values(std::make_tuple(Vector2{ 0.25f, 0.25f },
+                                                           Vector2{ 0.5f, 0.5f },
+                                                           uil::VerticalAlignment::Top,
+                                                           Vector2{ 0.25f, 0.25f }),
+                                           std::make_tuple(Vector2{ 0.25f, 0.25f },
+                                                           Vector2{ 0.5f, 0.5f },
+                                                           uil::VerticalAlignment::Mid,
+                                                           Vector2{ 0.25f, 0.5f }),
+                                           std::make_tuple(Vector2{ 0.25f, 0.25f },
+                                                           Vector2{ 0.5f, 0.5f },
+                                                           uil::VerticalAlignment::Bottom,
+                                                           Vector2{ 0.25f, 0.75f })));
+
+
+class VectorDoubleVerticalAlignmentFixtures : public testing::TestWithParam<uil::VerticalAlignment> { };
+
+TEST_P(VectorDoubleVerticalAlignmentFixtures, Succsess) {
+    auto const alignment = GetParam();
+
+    auto constexpr pos  = Vector2{ 0.25f, 0.25f };
+    auto constexpr size = Vector2{ 0.5f, 0.5f };
+
+    auto const temp   = uil::vertical_aligned_position(pos, size, alignment);
+    auto const result = uil::vertical_aligned_position_reversed(temp, size, alignment);
+
+    EXPECT_FLOAT_EQ(pos.x, result.x);
+    EXPECT_FLOAT_EQ(pos.y, result.y);
+    EXPECT_FLOAT_EQ(pos.x, temp.x);
+}
+
+INSTANTIATE_TEST_SUITE_P(ALIGNMENT,
+                         VectorDoubleVerticalAlignmentFixtures,
                          testing::Values(uil::VerticalAlignment::Top,
                                          uil::VerticalAlignment::Mid,
                                          uil::VerticalAlignment::Bottom));
