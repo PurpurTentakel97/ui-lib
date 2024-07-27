0214be75a955b1ec2530b76cd00befd4e9927180
Martin <martin.schreurs@netcologne.de>
2024-07-28 00:31:18 +0200

    check and update all the element inside a scene

--- src/scene.cpp
+++ src/scene.cpp
@@ -6,27 +6,26 @@
 #include <uil/context.hpp>
 #include <uil/element.hpp>
 #include <uil/scene.hpp>
-#include <ranges>
 
 namespace uil {
     bool Scene::check(Context const& context) const {
-        for (auto const& e : std::ranges::views::reverse(m_elements)) {
-            if (not e->check(context)) {
-                return false;
-            }
+        auto keep_checking = true;
+        for (auto const& e : m_elements) {
+            keep_checking &= e->check(context);
         }
         on_check.invoke(*this);
-        return true;
+        return keep_checking;
+
+
     }
 
     bool Scene::update(Context const& context) const {
-        for (auto const& e : std::ranges::views::reverse(m_elements)) {
-            if (not e->update(context)) {
-                return false;
-            }
+        auto keep_updating = true;
+        for (auto const& e : m_elements) {
+            keep_updating &= e->update(context);
         }
         on_update.invoke(*this);
-        return true;
+        return keep_updating;
     }
 
     bool Scene::render(Context const& context) const {
