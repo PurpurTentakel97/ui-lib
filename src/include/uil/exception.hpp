06e584f7ea002d8d26941f197464dbcc329dfa25
Martin <martin.schreurs@netcologne.de>
2024-07-28 21:18:01 +0200

    doc exception

--- src/include/uil/exception.hpp
+++ src/include/uil/exception.hpp
@@ -8,22 +8,40 @@
 #include <stdexcept>
 
 namespace uil {
+    /**
+     * use for any kind of error inside the uil::Callback class.
+     */
     class CallbackException final : public std::runtime_error {
         using std::runtime_error::runtime_error;
     };
 
+    /**
+     * use if a division would need to devide by zero.
+     */
     class DivideByZero final : public std::runtime_error {
         using std::runtime_error::runtime_error;
     };
 
+    /**
+     * use if a unexpected resolution ocures at certain operation.
+     * this mostly occures when the resolution enum does not have a predefined value.
+     */
     class BadResolution final : public std::logic_error {
         using std::logic_error::logic_error;
     };
 
+    /**
+     * use if an unexpected alignment ocures at certain operation.
+     * this mostly occures when the alignment enum does not have a predefined value.
+     */
     class BadAlignment final : public std::logic_error {
         using std::logic_error::logic_error;
     };
 
+    /**
+     * use if an unexpected movement ocures at certain operation.
+     * this mostly occures when the movement enum does not have a predefined value.
+     */
     class BadMovementType final : public std::logic_error {
         using std::logic_error::logic_error;
     };
