b34a64cb3f7bf940839e47f6315f8a72c1e10fd9
Martin <martin.schreurs@netcologne.de>
2024-07-28 19:17:23 +0200

    update doc link

--- src/include/uil/elements/link.hpp
+++ src/include/uil/elements/link.hpp
@@ -6,8 +6,9 @@
 #pragma once
 #include <uil/elements/text.hpp>
 
-/*
- * Provides a clickable Link
+/**
+ * provides a clickable link
+ * use the set-member-functions to configurate
  */
 namespace uil {
     class Link : public Text {
@@ -19,63 +20,74 @@ namespace uil {
 
     public:
         using Text::Text;
-
-        /*
-         * @param[in] color the link turns to when it was clicked
+        
+        /**
+         *
+         * @param color color the link turns to when it was clicked
          */
         void set_color_clicked(Color color);
-        /*
-         * @returns current color the link turns to whens it was clicked
+        /**
+         * 
+         * @return color the link turns to when it was clicked
          */
         [[nodiscard]] Color color_clicked() const;
 
-        /*
-         * @param[in] string that gets called when the link is clickd
-         * @see string will not be validated
+        /**
+         * string will not be validated
+         *
+         * @param link string that gets called when the link is clickd
          */
         void set_link(std::string link);
-        /*
-         * @returns current string that gets called when the links is clicked.
+        /**
+         *
+         * @return string that gets called when the link is clickd
          */
         [[nodiscard]] std::string link() const;
 
-        /*
-         * @param[in] if the link was clicked.
-         * @see this will change the link color
+        /**
+         * this sets the clicked status
+         *
+         * @param clicked if the link was clicked
          */
         void set_clicked(bool clicked);
-        /*
-         * @returns if the link was clicked.
+        /**
+         * 
+         * @return if the link was clicked
          */
         [[nodiscard]] bool clicked() const;
 
-        /*
-         * @param[in] if the link gets underlined when hovering.
+        /**
+         *
+         * @param render_hover if the link gets underlined when hovering
          */
         void set_render_hover(bool render_hover);
-        /*
-         * @returns if the links gets currently unterlind when hovering.
+        /**
+         * 
+         * @return if the link gets underlined when hovering
          */
         [[nodiscard]] bool render_hover() const;
 
-        /*
-         * calls Text to check
-         * calls Link when collider is hovered and left mouse button is clicked
-         * chances cursor to poiting finger when hovered
+        /**
+         * checks the current state
          *
+         * calls text to check.
+         * checks whether the text ist hovered or clicked.
+         * calls callbacks and link if it so.
          *
-         * @param[in] contains the context of all changes
-         * @return default: return value from Text::check()
-         * @return false when hovered or clicked
+         * @param context all changes of the last frameall changes of the last frame
+         * @return whether the next scene should keep checking or not
          */
         [[nodiscard]] bool check(Context const& context) override;
-        /*
-         * calls UIElement to render
-         * calls render_text() in Text
-         * render hover when activated
+
+        /**
+         * renders the current state
          *
-         * @param[in] contains the context of all changes
-         * @return return value from UIElement::render()
+         * calls UIEleent to render.
+         * calls Text to render text.
+         * renders hover when activated.
+         * 
+         * @param context all changes of the last frameall changes of the last frame
+         * @return whether the next scene should keep rendering or not
          */
         [[nodiscard]] bool render(Context const& context) const override;
     };
