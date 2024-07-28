814759bee09c96b85e7fc74d466ef2a3c7a3e926
Martin <martin.schreurs@netcologne.de>
2024-07-28 21:11:28 +0200

    doc context

--- src/include/uil/context.hpp
+++ src/include/uil/context.hpp
@@ -9,9 +9,22 @@
 #include <raylib.h>
 
 namespace uil {
+    /**
+     * provides all values for check, update, render and resize
+     */
     struct Context final {
         Vector2 mouse_position;
         Font const* font;
         cpt::Vec2_i resolution;
+
+        /**
+         * all values should be from the beginning of the frame
+         *
+         * @param _mouse_position current mouse position
+         * @param _font current font pointer
+         * @param _resolution current resolution
+         */
+        Context(Vector2 _mouse_position, Font const* _font, cpt::Vec2_i _resolution);
+        Context() = delete;
     };
 }
