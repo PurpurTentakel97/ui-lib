a9e93f0b7d8800483507b17fb302f902f9349483
Martin <martin.schreurs@netcologne.de>
2025-06-20 23:56:49 +0200

    feat: add button asset configuration with sound support to ButtonTestScene

--- program/button_test_scene.cpp
+++ program/button_test_scene.cpp
@@ -7,7 +7,36 @@
 #include <uil/elements/button.hpp>
 #include <uil/global/app_context.hpp>
 
+uil::ButtonAssetConfig ButtonTestScene::gen_button_asset_config() {
+    auto assets = uil::ButtonAssetConfig{};
+    auto& sound = uil::AppContext::instance().sound();
+
+    if (auto const result = sound.load_sound("/assets/sounds/clicked_accepted_std.mp3"); result.has_value()) {
+        assets.clicked_sound_id = result.value();
+    }
+
+    if (auto const result = sound.load_sound("/assets/sounds/clicked_disabled_std.mp3"); result.has_value()) {
+        assets.disabled_sound_id = result.value();
+    }
+
+    if (auto const result = sound.load_sound("/assets/sounds/clicked_press_std.mp3"); result.has_value()) {
+        assets.pressed_sound_id = result.value();
+    }
+
+    if (auto const result = sound.load_sound("/assets/sounds/clicked_release_std.mp3"); result.has_value()) {
+        assets.released_sound_id = result.value();
+    }
+
+    if (auto const result = sound.load_sound("/assets/sounds/hovered_std.mp3"); result.has_value()) {
+        assets.hovered_off_sound_id = result.value();
+        assets.hovered_on_sound_id  = result.value();
+    }
+
+    return assets;
+}
+
 ButtonTestScene::ButtonTestScene() {
+    // buttons
     m_button_1          = emplace_top<uil::Button>(Rectangle{ 0.5f, 0.5f, 0.2f, 0.1f }, uil::Alignment::MidMid);
     auto const locked_1 = m_button_1.lock();
     locked_1->on_clicked += [](auto const&) { cpt::log::r_info("Button Test Scene: Button 1: on_clicked"); };
@@ -24,6 +53,7 @@ ButtonTestScene::ButtonTestScene() {
     locked_2->on_disabled += [](auto const&) { cpt::log::r_info("Button Test Scene: Button 2: on_disabled"); };
     locked_2->disable();
 
+    // focus
     auto config_1     = uil::FocusConfig{};
     config_1.down     = m_button_2;
     config_1.next     = m_button_2;
@@ -37,4 +67,9 @@ ButtonTestScene::ButtonTestScene() {
     locked_2->set_focus_config(config_2);
 
     uil::AppContext::instance().focus().set_current(m_button_1);
+
+    // assets
+    auto const assets = gen_button_asset_config();
+    locked_1->set_asset_config(assets);
+    locked_2->set_asset_config(assets);
 }
--- program/button_test_scene.hpp
+++ program/button_test_scene.hpp
@@ -3,6 +3,7 @@
 //
 
 #pragma once
+#include <uil/elements/button.hpp>
 #include <uil/scene.hpp>
 
 namespace uil {
@@ -14,6 +15,8 @@ class ButtonTestScene final : public uil::Scene {
 private:
     std::weak_ptr<uil::Button> m_button_1;
     std::weak_ptr<uil::Button> m_button_2;
+
+    [[nodiscard]] static uil::ButtonAssetConfig gen_button_asset_config();
 
 public:
     ButtonTestScene();
