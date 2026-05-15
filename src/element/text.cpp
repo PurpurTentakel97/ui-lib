24b45d350913f27cad839e9aa007b15b53f1dcc1
Martin <martin.schreurs@netcologne.de>
2026-05-15 19:20:47 +0200

    feat: add Text element class and integrate into build system

---
+++ src/element/text.cpp
@@ -0,0 +1,8 @@
+//
+// Purpur Tentakel
+// 15.05.26
+//
+
+#include <uil/element/text/text.hpp>
+
+namespace uil { }
---
+++ src/include/uil/element/text/text.hpp
@@ -0,0 +1,11 @@
+//
+// Purpur Tentakel
+// 15.05.26
+//
+
+#pragma once
+#include <uil/global/element.hpp>
+
+namespace uil {
+    class Text : public UIElement { };
+} // namespace uil
